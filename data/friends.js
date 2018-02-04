4cc013d6cc9d8f8eae715cdbb75f13f06bdfb59b
Amit Rai <amit.rai.berkleybootcamp@gmail.com>
2018-02-04 10:33:36 -0800

    There is an error, square bracket is being inserted after scores[]

---
+++ data/friends.js
@@ -0,0 +1,37 @@
+var friends = [
+	{
+  	name:"Amit",
+  	photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/5/000/207/221/268d7e8.jpg",
+  	scores:[
+      	"5",
+      	"1",
+      	"4",
+      	"4",
+      	"5",
+      	"1",
+      	"2",
+      	"5",
+      	"4",
+      	"1"
+    	]
+	},
+	{
+  	name:"Rashmi",
+  	photo:"http://media.santabanta.com/gal/Bollywood/Rashmi-Sharma-Birthday-Celebration/rashmi-sharma-birthday-celebration-16_th.jpg",
+  	scores:[
+      	"5",
+      	"1",
+      	"4",
+      	"1",
+      	"1",
+      	"1",
+      	"2",
+      	"5",
+      	"4",
+      	"1"
+    	]
+	}
+
+];
+
+module.exports = friends;
